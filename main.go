e8a1575ecd22447ccbdd23ff79b039faec34f97a
Mehrdad Afshari <git@id.mehrdada.com>
2015-08-18 12:37:36 -0700

    count lines

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
+	//"go/ast"
 	"go/parser"
 	"go/token"
-	//	"io/ioutil"
+		"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -17,35 +17,37 @@ func main() {
 		basePath = os.Args[1]
 	}
 	fmt.Println(basePath)
-	var count int64 = 0
+	var lines int64 = 0
 	var fileCount int64 = 0
 	filepath.Walk(basePath, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() || !strings.HasSuffix(path, ".go") {
 			return nil
 		}
 		fset := token.NewFileSet()
-		file, err := parser.ParseFile(fset, path, nil, 0)
+		_, err = parser.ParseFile(fset, path, nil, 0)
 		//		fmt.Println(path)
 		if err != nil {
 			return nil
 		}
 
-		//		fileData, err := ioutil.ReadFile(path)
-		//		if err != nil {
-		//			return nil
-		//		}
+				fileData, err := ioutil.ReadFile(path)
+				if err != nil {
+					return nil
+				}
 		fileCount++
+		lines += int64(len(strings.Split(string(fileData), "\n")))
 
-		ast.Inspect(file, func(node ast.Node) bool {
+
+		/*ast.Inspect(file, func(node ast.Node) bool {
 			if _, ok := node.(*ast.CallExpr); ok {
 				count++
 				//				startPos, endPos := fset.Position(f.Pos()), fset.Position(f.End())
 				//				fmt.Println(string(fileData[startPos.Offset:endPos.Offset]))
 			}
 			return true
-		})
+		})*/
 		return nil
 	})
 
-	fmt.Printf("processed %d files, found %d call expressions\n", fileCount, count)
+	fmt.Printf("processed %d files, found %d lines\n", fileCount, lines)
 }
